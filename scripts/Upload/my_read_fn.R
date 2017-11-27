c5e694f594943b5e93276991666f46f7ad5615fe
JeanWenc <jeanwencelius@gmail.com>
2017-11-27 19:01:47 +0200

    Modifying the script that guesses encoding to fix bug with the Excel UTF-8 .csv file. Works fine!

--- scripts/Upload/my_read_fn.R
+++ scripts/Upload/my_read_fn.R
@@ -25,14 +25,19 @@ myReadFn<-function(data,header1,sep1,quote1,SaF){
   
   data1<-read.csv(file=data,header=header1,sep=sep1,quote = quote1,comment.char ="*",na.strings = c("NA",""),stringsAsFactors = SaF)  
   
-  enc<-guess_encoding(data,n_max = -1,threshold = 0.05)
-  if(length(enc)!=0){
-    if(length(grep("ISO",x = enc[1,1]))!=0 | length(grep("windows-12",x = enc[1,1]))){
-      data1<-read.csv(file=data,header=header1,sep=sep1,quote = quote1,comment.char ="*",na.strings = c("NA",""),stringsAsFactors = SaF,encoding = "latin1")
-      for(j in 1:dim(data1)[2]){
-        data1[,j]<-as.character(data1[,j])
-        for(i in 1:dim(data1)[1]){
-          data1[i,j]<-enc2utf8(data1[i,j])
+  temp<-data1[1,1]
+  if(length(grep(pattern = "\u00EF",x = temp))>0){
+    data1<-read.csv(file=data,header=header1,sep=sep1,quote = quote1,comment.char ="*",na.strings = c("NA",""),stringsAsFactors = SaF,fileEncoding = "UTF-8-BOM") 
+  }else{
+    enc<-guess_encoding(data,n_max = -1,threshold = 0.05)
+    if(length(enc)!=0){
+      if(length(grep("ISO",x = enc[1,1]))!=0 | length(grep("windows-12",x = enc[1,1]))){
+        data1<-read.csv(file=data,header=header1,sep=sep1,quote = quote1,comment.char ="*",na.strings = c("NA",""),stringsAsFactors = SaF,encoding = "latin1")
+        for(j in 1:dim(data1)[2]){
+          data1[,j]<-as.character(data1[,j])
+          for(i in 1:dim(data1)[1]){
+            data1[i,j]<-enc2utf8(data1[i,j])
+          }
         }
       }
     }
